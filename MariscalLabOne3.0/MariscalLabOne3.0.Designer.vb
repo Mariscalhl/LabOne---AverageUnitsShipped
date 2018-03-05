8cb04dd0865a90305163c7aa9603e32c782272f3
hlmar <hlmar@MSI>
2018-03-05 17:46:11 -0500

    Add project files.

---
+++ MariscalLabOne3.0/MariscalLabOne3.0.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMariscalLabOne
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lbAverageDisplay = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.tbUnitsOutput = New System.Windows.Forms.TextBox()
+        Me.lbDays = New System.Windows.Forms.Label()
+        Me.tbUnitInput = New System.Windows.Forms.TextBox()
+        Me.lbUnits = New System.Windows.Forms.Label()
+        Me.ToolTipAverageUnitsShipped = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'lbAverageDisplay
+        '
+        Me.lbAverageDisplay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbAverageDisplay.Location = New System.Drawing.Point(7, 182)
+        Me.lbAverageDisplay.Name = "lbAverageDisplay"
+        Me.lbAverageDisplay.Size = New System.Drawing.Size(267, 23)
+        Me.lbAverageDisplay.TabIndex = 24
+        Me.lbAverageDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.ToolTipAverageUnitsShipped.SetToolTip(Me.lbAverageDisplay, "The calculated average of the week goes here")
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(201, 226)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 23
+        Me.btnExit.Text = "E&xit"
+        Me.ToolTipAverageUnitsShipped.SetToolTip(Me.btnExit, "Exits the application")
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.Location = New System.Drawing.Point(109, 226)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 22
+        Me.btnReset.Text = "&Reset"
+        Me.ToolTipAverageUnitsShipped.SetToolTip(Me.btnReset, "Resets the form")
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(15, 226)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnter.TabIndex = 21
+        Me.btnEnter.Text = "&Enter"
+        Me.ToolTipAverageUnitsShipped.SetToolTip(Me.btnEnter, "Lets you input number into array")
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'tbUnitsOutput
+        '
+        Me.tbUnitsOutput.Location = New System.Drawing.Point(38, 32)
+        Me.tbUnitsOutput.Multiline = True
+        Me.tbUnitsOutput.Name = "tbUnitsOutput"
+        Me.tbUnitsOutput.ReadOnly = True
+        Me.tbUnitsOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.tbUnitsOutput.Size = New System.Drawing.Size(205, 138)
+        Me.tbUnitsOutput.TabIndex = 20
+        Me.ToolTipAverageUnitsShipped.SetToolTip(Me.tbUnitsOutput, "Shows the units for each day")
+        '
+        'lbDays
+        '
+        Me.lbDays.AutoSize = True
+        Me.lbDays.Location = New System.Drawing.Point(166, 4)
+        Me.lbDays.Name = "lbDays"
+        Me.lbDays.Size = New System.Drawing.Size(49, 17)
+        Me.lbDays.TabIndex = 19
+        Me.lbDays.Text = "&Day 1:"
+        Me.ToolTipAverageUnitsShipped.SetToolTip(Me.lbDays, "Shows the day that needs a units amount entered into")
+        '
+        'tbUnitInput
+        '
+        Me.tbUnitInput.Location = New System.Drawing.Point(76, 4)
+        Me.tbUnitInput.Name = "tbUnitInput"
+        Me.tbUnitInput.Size = New System.Drawing.Size(66, 22)
+        Me.tbUnitInput.TabIndex = 18
+        Me.ToolTipAverageUnitsShipped.SetToolTip(Me.tbUnitInput, "enter the units here")
+        '
+        'lbUnits
+        '
+        Me.lbUnits.AutoSize = True
+        Me.lbUnits.Location = New System.Drawing.Point(35, 4)
+        Me.lbUnits.Name = "lbUnits"
+        Me.lbUnits.Size = New System.Drawing.Size(44, 17)
+        Me.lbUnits.TabIndex = 17
+        Me.lbUnits.Text = "&Units:"
+        '
+        'frmMariscalLabOne
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(291, 255)
+        Me.Controls.Add(Me.lbAverageDisplay)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.tbUnitsOutput)
+        Me.Controls.Add(Me.lbDays)
+        Me.Controls.Add(Me.tbUnitInput)
+        Me.Controls.Add(Me.lbUnits)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmMariscalLabOne"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Average Units Shipped"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lbAverageDisplay As Label
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents tbUnitsOutput As TextBox
+    Friend WithEvents lbDays As Label
+    Friend WithEvents tbUnitInput As TextBox
+    Friend WithEvents lbUnits As Label
+    Friend WithEvents ToolTipAverageUnitsShipped As ToolTip
+End Class
---
+++ MariscalLabOne3.0/MariscalLabOne3.0.vb
@@ -0,0 +1,108 @@
+﻿Option Strict On
+
+''' <summary>
+''' Author: Hector Mariscal
+''' Project Name: Lab One - Average Units Shipped
+''' Date: 07-feb-2018
+''' Description: This application takes and validates units shipped for each day 
+''' of the week, and then at the end it caluclates the average units shipped.
+''' </summary>
+
+Public Class frmMariscalLabOne
+    'Form level declarations
+    Const minUnitSize As Integer = 0    'canstant fot the minimum size of units
+    Const maxUnitSize As Integer = 1000 'constant for the maximum size of units
+    Const dayInWeek As Integer = 7      ' constant for the number of days in a week
+
+    Dim calculatedAverage As Double = 0.0   'variable that holds the average that is calculated after the 7 days have been inputted
+    Dim unitSum As Double = 0.0 'variable that holds the sum of all of the units inputted
+    Dim userInput As String = String.Empty  'variable for the unvalidated user input
+    Dim validatedUserInput As Integer = 0   'variable for the validated user input
+
+
+    Dim counter As Integer = 0  'variable for the counter used in the if statements
+    Dim output As String = String.Empty 'variable that stores what will be outputted
+
+
+    Private unitArray(7) As Integer 'the array that holds all of the units for each day of the week
+    Private Sub frmMariscalLabOne_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    ''' <summary>
+    '''/This Is the event wiring for btnEnter it Is the primary function
+    '''of this form. The user input Is validated And then used in storing the input into an array and then calculate the average at the end of the 7 days.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+        userInput = tbUnitInput.Text
+        If (Integer.TryParse(userInput, validatedUserInput) AndAlso 'validates the user input and makes sure its an integer
+        validatedUserInput >= minUnitSize And validatedUserInput <= maxUnitSize) Then   'validates user input and makes sure the input is in range
+
+            counter = counter + 1                   'Counter set up to cylce through the array
+            unitArray(counter) = validatedUserInput
+            tbUnitInput.Text = ""
+            tbUnitInput.Focus()
+            output += unitArray(counter).ToString & " " & vbCrLf
+            lbDays.Text = "Day " & counter + 1 & ":"
+
+            If counter = dayInWeek Then     'When the counter is at 7 (for the days in the week) disables the buttons and text box and then goes to the for loop
+                tbUnitInput.Text = ""
+                tbUnitInput.Enabled = False
+                btnEnter.Enabled = False
+
+                For index As Integer = 0 To unitArray.Length - 1    'for loop to calculate the average of all of the numbers in the array
+                    unitSum = (unitSum + unitArray(index))
+                    calculatedAverage = Math.Round(unitSum / dayInWeek)
+
+                Next
+
+                If lbDays.Text = "Day " & 8 & ":" Then  'If statement that handles the days label and after the 7th day hides the label and disbles some of the controls as well as outputs the calculated average
+                    lbDays.Visible = False
+                    'MsgBox("The array is full", MsgBoxStyle.Information)
+                    lbAverageDisplay.Text = "Average per day: " & calculatedAverage.ToString
+                    'tbUnitInput.Enabled = False
+
+                End If
+
+            End If
+
+        Else
+            output += "The input must be a whole number between " & minUnitSize.ToString() & " and " & maxUnitSize.ToString() & " Please enter a whole number." & vbCrLf    'Tells the user of a non valid input and prompts them to enter a whole number
+            tbUnitInput.Focus()
+            tbUnitInput.Text = ""
+        End If
+        tbUnitsOutput.Text = output
+
+    End Sub
+    ''' <summary>
+    ''' This Is an event wiring for btnReset
+    '''It resets everything on the form back to the default state.
+    '''</summary>
+    '''<param name="sender"></param>
+    '''<param name="e"></param>
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        tbUnitsOutput.ResetText()
+        lbAverageDisplay.ResetText()
+        lbDays.Text = "Day 1:"
+        tbUnitInput.Text = ""
+        output = ""
+        tbUnitInput.Enabled = True
+        btnEnter.Enabled = True
+        tbUnitInput.Focus()
+        lbDays.Visible = True
+        Array.Clear(unitArray, 0, unitArray.Length)
+        counter = 0
+        calculatedAverage = 0
+        unitSum = 0
+    End Sub
+    ''' <summary>
+    ''' This Is an event wiring for btnExit
+    ''' It closes the form.
+    '''</summary>
+    '''<param name="sender"></param>
+    '''<param name="e"></param>
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
